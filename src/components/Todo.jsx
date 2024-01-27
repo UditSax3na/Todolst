838a1dcabdb0dc370d14bc1f2f226f37134aac24
Udit <uditsaxena940@gmail.com>
2024-01-27 17:28:30 +0530

    this is the second commit

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -9,9 +9,7 @@ export default function Todo(props) {
 
       <button className='btn btn-outline-success selection-done' onClick={()=>{props.update_(props.todo)}} ><Check className='done'/></button>
 
-
       <span className={props.todo['comp']===0?'todo':"todo active"} >{props.todo['heading']}</span>
-
 
       <button className='btn btn-outline-success selection-close' onClick={()=>{props.delete_(props.todo)}}><Cross className='close'/></button>
 
